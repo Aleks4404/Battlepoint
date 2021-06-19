bf7a53a3859a842872e585db17b14f9844b2de96
Aleksey Efimov <Aleks4404@mail.ru>
2021-06-19 20:48:32 +0400

    Проверка на падения теста CI Battlepoint.java

--- src/main/java/com/java24hours/Battlepoint.java
+++ src/main/java/com/java24hours/Battlepoint.java
@@ -43,11 +43,11 @@ public class Battlepoint {
     }
 
     private void createTargets() {
-        Point p1 = new Point(5,9);
+        Point p1 = new Point(5,90);
         targets.add(p1);
-        Point p2 = new Point(4,5);
+        Point p2 = new Point(4,50);
         targets.add(p2);
-        Point p3 = new Point(9,2);
+        Point p3 = new Point(9,20);
         targets.add(p3);
     }
 
